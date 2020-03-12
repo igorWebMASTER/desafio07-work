9a100ab8b7fac1722c246ca59427ee50b473444b
igorWebMASTER <igor_1917@hotmail.com>
2020-03-12 00:36:25 -0300

    some bugs appear to be solved

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FlatList } from 'react-native';
 
 import { connect } from 'react-redux';
-import { bindActionsCreators } from 'redux';
-// import  from '@react-native-community/async-storage';
+import { bindActionCreators } from 'redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import * as CardActions from '../../store/modules/cart/actions';
+import * as CartActions from '../../store/modules/cart/actions';
 
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 
 import {
+  Container,
   Product,
   ProductImage,
   ProductTitle,
@@ -36,7 +36,7 @@ class Main extends React.Component {
 
     const data = response.data.map(product => ({
       ...product,
-      priceFormated: formatPrice(product.price),
+      priceFormatted: formatPrice(product.price),
     }));
 
     this.setState({ products: data });
@@ -50,19 +50,47 @@ class Main extends React.Component {
 
   renderProduct = ({ item }) => {
     const { amount } = this.props;
+
     return (
       <Product key={item.id}>
         <ProductImage source={{ uri: item.image }} />
         <ProductTitle>{item.title}</ProductTitle>
-        <ProductPrice>{item.price}</ProductPrice>
+        <ProductPrice>{formatPrice(item.price)}</ProductPrice>
         <AddButton onPress={() => this.handleAddProduct(item.id)}>
           <ProductAmount>
-            <Icon name="add-shopping-cart" color="#fff" size={20} />
+            <Icon name="add-shopping-cart" color="#FFF" size={20} />
             <ProductAmountText>{amount[item.id] || 0}</ProductAmountText>
           </ProductAmount>
-          <AddButtonText> ADICIONAR</AddButtonText>
+          <AddButtonText>ADICIONAR</AddButtonText>
         </AddButton>
       </Product>
     );
   };
+
+  render() {
+    const { products } = this.state;
+    return (
+      <Container>
+        <FlatList
+          horizontal
+          data={products}
+          extraData={this.props}
+          keyExtractor={item => String(item.id)}
+          renderItem={this.renderProduct}
+        />
+      </Container>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
